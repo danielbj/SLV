f44296e41ac39948ac8d52622e6f87ce1e3a9725
gummimand <nthost15@student.aau.dk>
2015-12-05 17:24:04 +0100

    Lavede roulette funktions forslag

--- SLV.c
+++ SLV.c
@@ -10,6 +10,7 @@
 #include "stdlib.h"
 #include "string.h"
 #include "time.h"
+#include <assert.h>
 
 //Constants
 #define TEACHER_IDENTIFIER_LENGTH (3+1)
@@ -19,7 +20,7 @@
 #define MODULES_PR_DAY (6)
 #define DAYS_PR_WEEK (5)
 #define ELIMINATION_PART (0.5)
-#define MUTATION_PCT 0.10
+#define MUTATION_PART 0.10
 #define DAY_TO_SWAP1 1
 #define DAY_TO_SWAP2 5
 
@@ -68,10 +69,11 @@ struct week {
     struct day days[DAYS_PR_WEEK];
 };
 
-struct fitted_population {
+typedef struct fitted_population {
     int week_fitness;
     struct week *week_pointer;
-};
+    int roulette_part;
+} fitted_population_t;
 
 //Function prototypes
 struct week* initialize_weeks(int n);
@@ -79,17 +81,22 @@ void next_generation(struct week* population_pool, unsigned int n);
 int fitness_of_week(const struct week* individual);
 void print_fittest_week(const struct week* population_pool);
 int compare_fitness(const void *a, const void *b);
-struct fitted_population* init_array_fitness_weeks(struct week* population_pool, unsigned int n);
+void init_fitness_of_weeks(fitted_population_t *population_fitnesses,
+                              struct week* population_pool, unsigned int n);
+void assign_roulette_part(fitted_population_t *population_fitnesses, unsigned int n);
+void individual_picker(fitted_population_t *population_fitnesses,
+                       int *individuals_killed, int amount_killed,
+                       unsigned int n);
 int gene_rand_num(int amount_killed);
-void mutator(int j, int rand_1, struct fitted_population *population_fitnesses);
-void create_new_individual(int j, int rand_1, int rand_2, struct fitted_population *population_fitnesses);
+void mutator(int j, int rand_1, fitted_population_t *population_fitnesses);
+void create_new_individual(int j, int rand_1, int rand_2, fitted_population_t *population_fitnesses);
 
 //main function
 int main(int argc, char *argv[]) {
-    
-srand(1);    
-    
-    
+
+srand(1);
+
+
     return 0;
 }
 
@@ -106,62 +113,108 @@ struct week* initialize_weeks(int n) {
 //Function that generates a new population.
 //This function overwrites the initial population (thus returning void).
 void next_generation(struct week* population_pool, unsigned int n) {
-    int j, random_number_1=-123, random_number_2=-123,
-        amount_killed=0;
-    struct fitted_population *population_fitnesses;
+    int i, j, random_number_1=-123, random_number_2=-123,
+        amount_killed=0, amount_living=0, biggest_roulette_part=0,
+        *individuals_killed;
+    fitted_population_t *population_fitnesses;
 
-    population_fitnesses = init_array_fitness_weeks(population_pool,n);
+    init_fitness_of_weeks(population_fitnesses, population_pool, n);
 
     amount_killed = n*ELIMINATION_PART;
+    amount_living = n - amount_killed;
 
-    //sorts the array, so that the worst individuals are placed first.
-    qsort(population_fitnesses,n,sizeof(struct fitted_population),compare_fitness);
+    individuals_killed = (int *)malloc(sizeof(int)*amount_killed);
+    assert(individuals_killed != NULL);
+
+    //sorts the array, so that the fittest individuals are placed first.
+    qsort(population_fitnesses,n,sizeof(fitted_population_t),compare_fitness);
+
+    assign_roulette_part(population_fitnesses,n);
+
+    individual_picker(population_fitnesses, individuals_killed, amount_killed, n);
 
     //Generates new population to fill out killed individuals.
-    //Creating from the surviving indivduals.
+    //Creating from the surviving indivduals. FIX
     for(j=0; j < amount_killed; j++){
-        random_number_1 = gene_rand_num(amount_killed); 
-        random_number_2 = gene_rand_num(amount_killed);
+        random_number_1 = gene_rand_num(amount_living);
+        random_number_2 = gene_rand_num(amount_living);
 
-        if(random_number_1 < (n * MUTATION_PCT) + amount_killed){ //New mutated individual.
-            mutator(j, random_number_1, population_fitnesses);
+        //New mutated individual.
+        if(random_number_1 < (n * MUTATION_PART)){
+            mutator(individuals_killed[j], random_number_1, population_fitnesses);
         }
-        else{ //New individual
-            create_new_individual(j, random_number_1, random_number_2, population_fitnesses);
+        //New individual
+        else{
+            create_new_individual(individuals_killed[j], random_number_1, random_number_2, population_fitnesses);
         }
 
     }
 
     free(population_fitnesses);
+    free(individuals_killed);
 }
-
-int compare_fitness(const void *a, const void *b){
-    struct fitted_population *ca = (struct fitted_population *)a;
-    struct fitted_population *cb = (struct fitted_population *)b;
-    
-        return ca->week_fitness - cb->week_fitness;
-}
-
-struct fitted_population *init_array_fitness_weeks(struct week* population_pool, unsigned int n){
+void init_fitness_of_weeks(fitted_population_t *population_fitnesses,
+                              struct week* population_pool, unsigned int n){
     int i;
-    struct fitted_population *population_fitnesses;
 
-    population_fitnesses = (struct fitted_population *)malloc(sizeof(struct fitted_population) * n);
+    population_fitnesses = (fitted_population_t *)malloc(sizeof(fitted_population_t) * n);
     //Fills array with fitnesses.
     for(i=0; i < n; i++){
-        population_fitnesses[i].week_fitness = fitness_of_week(&population_pool[i]); 
+        population_fitnesses[i].week_fitness = fitness_of_week(&population_pool[i]);
         population_fitnesses[i].week_pointer = &population_pool[i];
     }
-
-    return population_fitnesses;
 }
 
-//Generates random number among the weeks that wont be killed.
-int gene_rand_num(int amount_killed){
-    return rand()%amount_killed + amount_killed;
+//Gives every individual a share of the roulette to be eliminated from.
+//https://en.wikipedia.org/wiki/Fitness_proportionate_selection ***KILDE***
+void assign_roulette_part(fitted_population_t *population_fitnesses, unsigned int n){
+    int i,j, total_fitness_of_weeks = 0;
+
+    //Calculates the total fitness for this generation
+    for (i = 0; i < n; i++) {
+        total_fitness_of_weeks += population_fitnesses[i].week_fitness;
+    }
+
+    for (i = 0; i < n; i++) {
+        population_fitnesses[i].roulette_part = 1/(population_fitnesses[i].week_fitness/
+                                                         total_fitness_of_weeks);
+    }
 }
 
-void mutator(int j, int rand_1, struct fitted_population *population_fitnesses){
+//Picks individuals with bigger value than the random number, which is constantly changed
+//Individuals picked till amount_killed reached.
+void individual_picker(fitted_population_t *population_fitnesses,
+                       int *individuals_killed, int amount_killed,
+                       unsigned int n){
+    int i,j, selector = 0, biggest_roulette_part = 0;
+
+    biggest_roulette_part = population_fitnesses[n-1].roulette_part;
+
+    for(i=0; i < amount_killed;){
+        for(j=0; j < n; j++){
+            selector = gene_rand_num(biggest_roulette_part)+1;
+            if(selector < population_fitnesses[j].roulette_part)
+                individuals_killed[i] = j;
+                i++;
+        }
+    }
+}
+
+//sorts the array (most fit to least fit)
+int compare_fitness(const void *a, const void *b){
+    fitted_population_t *ca = (fitted_population_t *)a;
+    fitted_population_t *cb = (fitted_population_t *)b;
+
+        return cb->week_fitness - ca->week_fitness;
+}
+
+//Generates random number from 0 to input-1.
+int gene_rand_num(int n){
+    return rand()%n;
+}
+
+//Mutates individual by swapping 2 days in a random week chosen.
+void mutator(int j, int rand_1, fitted_population_t *population_fitnesses){
     int d;
     struct week temporary_week;
 
@@ -173,20 +226,21 @@ void mutator(int j, int rand_1, struct fitted_population *population_fitnesses){
     //Mutation
     temporary_week.days[DAY_TO_SWAP1] = population_fitnesses[rand_1].week_pointer->days[DAY_TO_SWAP2];
     temporary_week.days[DAY_TO_SWAP2] = population_fitnesses[rand_1].week_pointer->days[DAY_TO_SWAP1];
-    
-    //make new individual consisting of a good week, with swapped days.
+
+    //kills individual j and makes new individual, with swapped days.
     population_fitnesses[j].week_pointer[0] = temporary_week; //FIX week_pointer (evt fyld alle dage)
 }
 
-void create_new_individual(int j, int rand_1, int rand_2, struct fitted_population *population_fitnesses){
+//Function makes new individual from 2 random weeks. //FIX Lav bedre udvælgelse af gener.
+void create_new_individual(int j, int rand_1, int rand_2, fitted_population_t *population_fitnesses){
     int d, m;
     struct week temporary_week;
 
-    //These loops mixes days and modules into a new week.
+    //These loops mixes days and modules into a new week(individual).
     for(d=0; d < DAYS_PR_WEEK; d++){
-        for(m=0; m < MODULES_PR_DAY; m++){ 
+        for(m=0; m < MODULES_PR_DAY; m++){
             if(0 <= d && d < 2){
-                if(0 <= m && m <3){
+                if(0 <= m && m < 3){
                     temporary_week.days[d].modules[m] = population_fitnesses[rand_1].week_pointer->days[d].modules[m];
                 }
                 else{
@@ -204,7 +258,7 @@ void create_new_individual(int j, int rand_1, int rand_2, struct fitted_populati
         }
     }
      //make new individual on element j, which is being discarded.
-    population_fitnesses[j].week_pointer[0] = temporary_week; //FIX
+    population_fitnesses[j].week_pointer[0] = temporary_week; //FIX week_pointer (evt fyld alle dage)
 }
 
 //Function for finding fitness of one week.
@@ -218,19 +272,3 @@ int fitness_of_week(const struct week* individual) {
 void print_fittest_week(const struct week* population_pool) {
     return;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

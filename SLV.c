10fbf88ea227a43ea9094587fb2fc892bc45854e
gummimand <nthost15@student.aau.dk>
2015-12-08 19:36:24 +0100

    Update SLV.c

--- SLV.c
+++ SLV.c
@@ -10,6 +10,7 @@
 #include "stdlib.h"
 #include "string.h"
 #include "time.h"
+#include <assert.h>
 
 //Constants
 #define TEACHER_IDENTIFIER_LENGTH (3+1)
@@ -18,6 +19,7 @@
 #define JOBS_PR_MODULE (15)
 #define MODULES_PR_DAY (6)
 #define DAYS_PR_WEEK (5)
+#define MAX_SUBJECT_CHARS (15)
 
 //Enumerations
 enum subjects {
@@ -69,15 +71,14 @@ struct week* initialize_weeks(int n);
 void next_generation(struct week* population_pool, unsigned int n);
 int fitness_of_week(const struct week* individual);
 void print_fittest_week(const struct week* population_pool);
+void output(struct week result_week);
+char *enum_to_string(enum subjects subject);
 
 
 
 //main function
-int main(int argc, char *argv[]) {
-    
-    
-    
-    
+int main(int argc, char *argv[]){
+
     return 0;
 }
 
@@ -111,17 +112,73 @@ void print_fittest_week(const struct week* population_pool) {
     return;
 }
 
+void output(struct week result_week){
+    int d,m,j;
+    char temp_string_subject[MAX_SUBJECT_CHARS];
+    FILE *out_ptr;
 
+    out_ptr = fopen("Optimal_schedule.txt", "w");
+    assert(out_ptr != NULL);
 
+    fprintf(out_ptr, "The schedule:\n\n");
+    fprintf(out_ptr, "%s%40s%40s%40s%40s\n" "", "TUE", "WED", "THU", "FRI");
+    for(d=0; d < DAYS_PR_WEEK; d++){
+        for(m=0; m < MODULES_PR_DAY; m++){
+            for (j=0; j < JOBS_PR_MODULE; j++){
+                strcpy(temp_string_subject, enum_to_string(result_week.days[d].modules[m].jobs[j].subject));
+                fprintf(out_ptr, "%20.20s   %5.5s   %4.4s\n",
+                       temp_string_subject, result_week.days[d].modules[m].jobs[j].class,
+                       result_week.days[d].modules[m].jobs[j].teacher[0]);
+            }
+            fprintf(out_ptr, "____________________\n");
+        }
+    }
+}
+char *enum_to_string(enum subjects subject){
+    char temp_string[MAX_SUBJECT_CHARS];
 
+    switch (subject) {
+        case art:
+            strcpy(temp_string, "billedkunst"); break;
+        case biology:
+            strcpy(temp_string, "biologi"); break;
+        case danish:
+            strcpy(temp_string, "dansk"); break;
+        case english:
+            strcpy(temp_string, "engelsk"); break;
+        case physics:
+            strcpy(temp_string, "fysik"); break;
+        case geography:
+            strcpy(temp_string, "geografi"); break;
+        case history:
+            strcpy(temp_string, "historie"); break;
+        case crafting:
+            strcpy(temp_string, "håndarbejde"); break;
+        case phys_ed:
+            strcpy(temp_string, "idræt"); break;
+        case classtime:
+            strcpy(temp_string, "klassens time"); break;
+        case religion:
+            strcpy(temp_string, "kristendomskundskab"); break;
+        case cooking:
+            strcpy(temp_string, "hjemkundskab"); break;
+        case math:
+            strcpy(temp_string, "matematik"); break;
+        case music:
+            strcpy(temp_string, "musik"); break;
+        case nature:
+            strcpy(temp_string, "natur-teknik"); break;
+        case socialstud:
+            strcpy(temp_string, "samfundsfag"); break;
+        case woodwork:
+            strcpy(temp_string, "sløjd"); break;
+        case german:
+            strcpy(temp_string, "tysk"); break;
+        case elective:
+            strcpy(temp_string, "valgfag"); break;
+        case prep:
+            strcpy(temp_string, "forberedelsestid"); break;
+    }
 
-
-
-
-
-
-
-
-
-
-
+    return temp_string;
+}

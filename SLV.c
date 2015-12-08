8d07105c58dad58e302fde46506dc5c06b5406f8
danielbj <daniel.broder.jensen@gmail.com>
2015-12-09 00:32:21 +0100

    Wrote function for finding teacher conflicts.

--- SLV.c
+++ SLV.c
@@ -73,6 +73,7 @@ int insert_job_in_week(struct job* job, struct week* week, int day, int module);
 int insert_job_in_module(struct job* job, struct module* module);
 int is_empty_job(struct job* job);
 int is_teacher_conflict(struct module* module, struct job* job);
+int is_class_conflict(struct module* module, struct job* job);
 
 void next_generation(struct week* population_pool, unsigned int n);
 int fitness_of_week(const struct week* individual);
@@ -82,10 +83,10 @@ void print_fittest_week(const struct week* population_pool);
 
 //main function
 int main(int argc, char *argv[]) {
-    
-    
-    
-    
+
+
+
+
     return 0;
 }
 
@@ -95,16 +96,16 @@ int main(int argc, char *argv[]) {
 //Initialize n weeks with jobs from jobs.txt
 //Returns NULL if error.
 struct week* initialize_weeks(int number_of_weeks) {
-    
+
     //FUNCTION: Read data from file to local structs
-    
-    
-    
-    
+
+
+
+
     //FUNCTION: Randomly insert, regarding hard limits
-    
-    
-    
+
+
+
     return 0;
 }
 
@@ -113,7 +114,7 @@ struct week* initialize_weeks(int number_of_weeks) {
 //Function for reading data from jobs.txt to jobs structure.
 //Insures that last element is NULL (teacher == 0).
 struct job* read_jobs() {
-    
+
     return 0;
 }
 
@@ -123,26 +124,26 @@ struct job* read_jobs() {
 //Returns NULL on error.
 struct week* insert_jobs(struct job* job_pool, int number_of_weeks) {
     int i;//loop counter
-    
+
     //Allocate memory for pool of generated weeks
     struct week* week_pool = calloc(number_of_weeks, sizeof(struct week));
     if (!week_pool) {
         return 0;
     }
-    
+
     //seed random number generator
     srand(time(0));
-    
+
     //Generate n weeks
     for (i = 0; i < number_of_weeks; i++) {
         if (!generate_week(&week_pool[i], job_pool)) {
             return 0;
         }
     }
-    
+
     //Free the job pool
     free(job_pool);
-    
+
     //Return pool of weeks
     return week_pool;
 }
@@ -154,18 +155,18 @@ struct week* insert_jobs(struct job* job_pool, int number_of_weeks) {
 int generate_week(struct week* week, struct job* job_pool) {
     int random_day, random_module;//For storing random indexes
     int i = 0;//job pool index
-    
+
     while (!is_empty_job(&job_pool[i])) {
         random_day = rand() % DAYS_PR_WEEK;//Select a random day
         random_module = rand() % MODULES_PR_DAY;//Select a random module
-        
+
         if (!insert_job_in_week(&job_pool[i], week, random_day, random_module)) {
             return 0;
         } else {
             i++;
         }
     }
-    
+
     return 1;
 }
 
@@ -176,7 +177,7 @@ int generate_week(struct week* week, struct job* job_pool) {
 int insert_job_in_week(struct job* job, struct week* week, int day, int module) {
     int d_day, d_module;//Delta-values for offsetting day and module.
     struct module* current_module;//For holding the module currently being tested for insertion.
-    
+
     for (d_day = 0; d_day < DAYS_PR_WEEK; d_day++) {
         for (d_module = 0; d_module < MODULES_PR_DAY; d_module++) {
             current_module = &week->days[(day + d_day) % DAYS_PR_WEEK].modules[(module + d_module) % MODULES_PR_DAY];
@@ -185,22 +186,22 @@ int insert_job_in_week(struct job* job, struct week* week, int day, int module) 
             }
         }
     }
-    
+
     return 0;
 }
 
 
-//Inserts a job in a module.
+//Inserts a job in a module. Will respect teacher and class conflicts.
 //Returns NULL if error.
 int insert_job_in_module(struct job* job, struct module* module) {
-    
+
     int i;//loop counter
-    
-    //Check for teacher conflicts.
-    if (is_teacher_conflict(module, job)) {
+
+    //Check for teacher and class conflicts.
+    if (is_teacher_conflict(module, job) || is_class_conflict(module, job)) {
         return 0;
     }
-    
+
     //Insert job in first free entry
     for (i = 0; i < JOBS_PR_MODULE; i++) {
         if (is_empty_job(&module->jobs[i])) {
@@ -208,7 +209,7 @@ int insert_job_in_module(struct job* job, struct module* module) {
             return 1;
         }
     }
-    
+
     return 0;
 }
 
@@ -222,10 +223,33 @@ int is_empty_job(struct job* job) {
 
 
 
-//Function for checking for teacher conflicts.
+//Function for checking for teacher conflicts in a module.
 //Returns !NULL if conflict.
 int is_teacher_conflict(struct module* module, struct job* job) {
-    
+    int d_job;//Job index offset
+    int d_teacher;//Teacher index offset
+
+    for (d_job = 0; d_job < JOBS_PR_MODULE; d_job++) {
+        for (d_teacher = 0; d_teacher < TEACHERS_PR_JOB; d_teacher++) {
+            if () {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+
+//Function for checking for class conflicts in a module.
+//Returns !NULL if conflict.
+int is_class_conflict(struct module* module, struct job* job) {
+    int d_job;//job index offset
+
+    for (d_day = 0; d_day < JOBS_PR_MODULE; d_day++) {
+        //TODO
+    }
 }
 
 
@@ -258,18 +282,3 @@ int fitness_of_week(const struct week* individual) {
 void print_fittest_week(const struct week* population_pool) {
     return;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

50b8e2bac567057c13cb828e1de078b587a70668
gummimand <nthost15@student.aau.dk>
2015-12-10 23:34:38 +0100

    worked on teacher input, though unable to run .exe..

--- SLV.c
+++ SLV.c
@@ -6,10 +6,10 @@
 //
 
 //Includes
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "time.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <assert.h>
 
 //Constants
@@ -70,69 +70,68 @@ struct week {
 struct week* initialize_weeks(int n);
 void next_generation(struct week* population_pool, unsigned int n);
 int fitness_of_week(const struct week* individual);
-void print_fittest_week(const struct week* population_pool);
-void output(struct week result_week);
+void print_fittest_week(const struct week* fittest_week, char* teacher);
 char *enum_to_string(enum subjects subject);
 
 
 
-//main function
+//main function VIL IKKE KØRE ORDENTILIGT
 int main(int argc, char *argv[]){
+struct week a_week;
+printf("stsdas\n");
+/*
+strcpy(a_week.days[0].modules[0].jobs[0].teacher[0], "AH");
+    a_week.days[0].modules[0].jobs[0].subject = art;
+    strcpy(a_week.days[0].modules[0].jobs[0].class, "7.");
+strcpy(a_week.days[1].modules[1].jobs[0].teacher[0], "AH");
+    a_week.days[1].modules[1].jobs[0].subject = biology;
+    strcpy(a_week.days[1].modules[1].jobs[0].class, "8.");
+strcpy(a_week.days[2].modules[3].jobs[0].teacher[0], "AH");
+    a_week.days[2].modules[3].jobs[0].subject = danish;
+    strcpy(a_week.days[2].modules[3].jobs[0].class, "9.");
+strcpy(a_week.days[3].modules[5].jobs[0].teacher[0], "AH");
+    a_week.days[3].modules[5].jobs[0].subject = music;
+    strcpy(a_week.days[3].modules[5].jobs[0].class, "1.");
+strcpy(a_week.days[4].modules[7].jobs[0].teacher[0], "AH");
+    a_week.days[4].modules[7].jobs[0].subject = math;
+    strcpy(a_week.days[4].modules[7].jobs[0].class, "3.");
 
+print_fittest_week(&a_week, "AH");
+*/
     return 0;
 }
 
-
-
-//Input + generation + sanitation
-//Initialize n weeks with jobs from jobs.txt
-struct week* initialize_weeks(int n) {
-    return;
-}
-
-
-
-//Function that generates a new population.
-//This function overwrites the initial population (thus returning void).
-void next_generation(struct week* population_pool, unsigned int n) {
-    return;
-}
-
-
-
-//Function for finding fitness of one week.
-int fitness_of_week(const struct week* individual) {
-    return;
-}
-
-
-
 //Output the fittest of weeks in the population pool.
-void print_fittest_week(const struct week* population_pool) {
-    return;
-}
-
-void output(struct week result_week){
-    int d,m,j;
+void print_fittest_week(const struct week* fittest_week, char* teacher) {
+    int d,m,j,t, status = -123 ;
     char temp_string_subject[MAX_SUBJECT_CHARS];
     FILE *out_ptr;
 
     out_ptr = fopen("Optimal_schedule.txt", "w");
     assert(out_ptr != NULL);
 
-    fprintf(out_ptr, "The schedule:\n\n");
+    fprintf(out_ptr, "The schedule for %s is as following:\n\n", teacher);
     fprintf(out_ptr, "%s%40s%40s%40s%40s\n" "MAN", "TUE", "WED", "THU", "FRI");
-    for(d=0; d < DAYS_PR_WEEK; d++){
-        for(m=0; m < MODULES_PR_DAY; m++){
-            for (j=0; j < JOBS_PR_MODULE; j++){
-                strcpy(temp_string_subject, enum_to_string(result_week.days[d].modules[m].jobs[j].subject));
-                fprintf(out_ptr, "%20.20s   %5.5s   %4.4s\n",
-                       temp_string_subject, result_week.days[d].modules[m].jobs[j].class,
-                       result_week.days[d].modules[m].jobs[j].teacher[0]);
+    //Loop checks every day, module and job for the teacher to print schedule for
+    for(m=0; m < MODULES_PR_DAY; m++){
+        for (j=0; j < JOBS_PR_MODULE; j++){
+            for(d=0; d < DAYS_PR_WEEK; d++){
+                for (t = 0; t < TEACHERS_PR_JOB; t++) {
+                    if (strcmp(teacher, fittest_week->days[d].modules[m].jobs[j].teacher[t])){
+                        strcpy(temp_string_subject, enum_to_string(fittest_week->days[d].modules[m].jobs[j].subject));
+                        status = fprintf(out_ptr, "%20s   %5s %15s",
+                               temp_string_subject, fittest_week->days[d].modules[m].jobs[j].class,
+                               " ");
+                        printf("%d\n", status);
+                        //fseek(out_ptr, 15, SEEK_CUR); Designet til at layout passer så der skrives under hver dag hvis ikke space løsning virker
+                    }
+                }
             }
-            fprintf(out_ptr, "____________________\n");
         }
+        fprintf(out_ptr, "________________________________________\n");
     }
+
+    fprintf(out_ptr, "Week printed for given teacher\n");
 }
 char *enum_to_string(enum subjects subject){
     char temp_string[MAX_SUBJECT_CHARS];

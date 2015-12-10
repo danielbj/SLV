45655fc4309bedab204c73a717256d625173cf68
danielbj <daniel.broder.jensen@gmail.com>
2015-12-10 12:18:46 +0100

    reworked input. Bugs in IO.

--- SLV.c
+++ SLV.c
@@ -18,6 +18,9 @@
 #define JOBS_PR_MODULE (15)
 #define MODULES_PR_DAY (6)
 #define DAYS_PR_WEEK (5)
+#define MAX_SUBJECT_CHARS (20)
+#define EXPECTED_MIN_SCANS (4)
+#define JOB_FILE_NAME "jobs.txt"
 
 //Enumerations
 enum subjects {
@@ -42,7 +45,6 @@ enum subjects {
     elective,    //valgfag
     prep        //forberedelsestid
 };
-
 
 //Structures
 struct job {
@@ -71,22 +73,19 @@ int fitness_of_week(const struct week* individual);
 void print_fittest_week(const struct week* population_pool);
 
 //prototypes for readerfunction
-int job_counter(void);
-void file_reader(struct job *pool_jobs);
-void clean_and_seperate_teachers(char temp_teacher[]);
-enum subjects translator(char temp_subject[]);
-
-struct job *pool_jobs;
+struct job* read_jobs(void);
+int read_job(struct job* job, FILE* file);
+struct job* job_counter(FILE* file);
+enum subjects translate_subject(char temp_subject[]);
 
 //main function
-int main(int argc, char *argv[]) {    
-    //struct all_jobs{
-       //struct job pool_jobs[job_counter()+1];
-    //};
-    
-    file_reader(struct job *pool_jobs);
+int main(int argc, char *argv[]) {
 
-return 0;
+    read_jobs();
+
+
+
+    return 0;
 }
 
 
@@ -95,188 +94,183 @@ return 0;
 //Initialize n weeks with jobs from jobs.txt
 //Returns NULL if error.
 struct week* initialize_weeks(int number_of_weeks) {
-    
+
     //FUNCTION: Read data from file to local structs
-    
-    
-    
-    
+
+
+
+
     //FUNCTION: Randomly insert, regarding hard limits
-    
-    
-    
+
+
+
     return 0;
 }
 
+//Function for reading all jobs from a file
+//Returns NULL if error.
+struct job* read_jobs(void) {
+    int job = 0;//job pool index
+    int done = 0;//while-loop condition flag
+    int n_insertions;//number of insertions
 
-
-//Function for reading data from jobs.txt to jobs structure.
-//Insures that last element is NULL (teacher == 0).
-
-int job_counter(void) {
-    FILE *new_text;
-    new_text = fopen("jobs_comma","r");
-    
-    if(new_text == NULL) {
-    printf("Error, could not read file");
-    };
-    
-    int all = 0;
-    int new;
-    while (fscanf(new_text, "%d", &new) != EOF) {
-        all += new;
+    //DEBUG
+    FILE* file = fopen("jobs", "r");
+    if (!file) {
+        return 0;
     }
-    struct job *pool_jobs;
-    pool_jobs = (struct pool_jobs*) malloc(sizeof(struct job)*all);
-    
-return 0;
-} 
 
-
-void file_reader(struct job *pool_jobs) {
-    FILE *raw_text;
-    raw_text = fopen("jobs_comma","r");
-    
-    if(raw_text == NULL) {
-    printf("Error, could not read file");
-    };
-    
-    int g;
-    char buffer[200];
-    while (fgets(buffer, sizeof(buffer), raw_text) != EOF) {
-        
-        int temp_hour = 0;
-        char temp_subject[20];
-        char temp_class[20];
-        char temp_teacher[20];
-        
-        sscanf(buffer,"%d %s %s %s", &temp_hour, temp_subject,
-                                     temp_class, temp_teacher);
-        //cleans buffer
-        memset(buffer, '\0', sizeof(buffer));
-        
-        //finds individual teachers
-        char all_teachers[TEACHERS_PR_JOB][TEACHER_IDENTIFIER_LENGTH];
-        clean_and_seperate_teachers(temp_teacher);
-        
-        //translator from char to enum
-        enum subjects translator(char temp_subject[]);
-        
-        // function for copying into all_jobs
-        int i;
-        for(i = 0; i < temp_hour; i++) {
-            pool_jobs.teacher[g] = all_teachers;
-            pool_jobs.class[g] = temp_class;
-            pool_jobs.subject[g] = trans_subject;
-            g++
-        };
-    
+    struct job* job_pool = job_counter(file);
+    if (!job_pool) {
+        fclose(file);
+        return 0;
     }
-    
-    //places NULL at the end of pool_jobs
-    pool_jobs.teacher[g+1] = NULL;
-    pool_jobs.class[g+1] = NULL;
-    pool_jobs.subject[g+1] = NULL;
-    
-return 0;
-}
 
-void clean_and_seperate_teachers(temp_teacher) {
-    //cleans temp_teacher of commas
-    int h;
-    char space = " ";
-    char comma = ",";
-    for(h = 0; h < sizeof(temp_teacher); h++) {
-        if(temp_teacher[h] == comma) {
-        temp_teacher[h] = space;
+    while (!done) {
+        n_insertions = read_job(&job_pool[job], file);
+        if (!n_insertions) {
+            done = 1;
+        } else {
+            job += n_insertions;
         }
     }
-        
-   //teacher seperator
-   char seperat_teachers[TEACHERS_PR_JOB][TEACHER_IDENTIFIER_LENGTH];
-   sscanf(temp_teacher, "%s %s %s", seperat_teachers[0][0], 
-                                    seperat_teachers[1][0], 
-                                    seperat_teachers[2][0]);
-        
-   memcpy(all_teachers,seperat_teachers, 
-   TEACHERS_PR_JOB*TEACHER_IDENTIFIER_LENGTH*sizeof(seperat_teachers));
 
-return 0;
+    fclose(file);
+    return job_pool;
+}
+
+//Function for reading one job from a file.
+//Returns NULL if EOF, else returns number of inserted jobs in job pool.
+int read_job(struct job* job, FILE* file) {
+    int i;//loop counter
+    int status = 0;
+    int n_insertions;
+    struct job temp_job;
+    char temp_subject[MAX_SUBJECT_CHARS];
+
+    //Nullify teachers
+    for (i = 0; i < TEACHERS_PR_JOB; i++) {
+        temp_job.teacher[i][0] = 0;
+        temp_job.teacher[i][TEACHER_IDENTIFIER_LENGTH - 1] = 0;
+    }
+
+    //Read one line from the file
+    status = fscanf(file, "%d %s %s %s %s %s \n",
+        &n_insertions,
+        temp_subject,
+        temp_job.class,
+        temp_job.teacher[0],
+        temp_job.teacher[1],
+        temp_job.teacher[2]
+    );
+
+    //DEBUG:
+    printf("\n%d\t%s\t%s\t%s\t%s\t%s\n",
+        n_insertions,
+        temp_subject,
+        temp_job.class,
+        temp_job.teacher[0],
+        temp_job.teacher[1],
+        temp_job.teacher[2]
+    );
+
+    //Return if EOF
+    if (status < EXPECTED_MIN_SCANS) {
+        return 0;
+    }
+
+    //Translate the subject to enum
+    temp_job.subject = translate_subject(temp_subject);
+
+    for (i = 0; i < n_insertions; i++) {
+        job[i] = temp_job;
+    }
+
+    return n_insertions;
+
+}
+
+//For calculating the size of the job pool
+//Returns NULL on error.
+struct job* job_counter(FILE* file) {
+    int n_jobs = 0;
+    int new;
+    while (fscanf(file, "%d", &new) != EOF) {
+        n_jobs += new;
+    }
+
+    struct job* job_pool = malloc(sizeof(struct job) * (n_jobs+1));
+
+    return job_pool;
 }
 
 //translate from char to enum
-enum subjects translator(char temp_subject[]) {
+enum subjects translate_subject(char temp_subject[]) {
     enum subjects trans_subject;
-    
+
     if(strncmp(temp_subject, "Billedkunst", 4) == 0) {
-    trans_subject = art;
+        trans_subject = art;
     }
     else if(strncmp(temp_subject, "Biologi", 4) == 0) {
-    trans_subject = biology;
+        trans_subject = biology;
     }
     else if(strncmp(temp_subject, "Dansk", 4) == 0) {
-    trans_subject = danish;
+        trans_subject = danish;
     }
     else if(strncmp(temp_subject, "Engelsk", 4) == 0) {
-    trans_subject = english;
+        trans_subject = english;
     }
     else if(strncmp(temp_subject, "Fysik/kemi", 4) == 0) {
-    trans_subject = physics;
+        trans_subject = physics;
     }
     else if(strncmp(temp_subject, "Geografi", 4) == 0) {
-    trans_subject = geography;
+        trans_subject = geography;
     }
     else if(strncmp(temp_subject, "Historie", 4) == 0) {
-    trans_subject = history;
+        trans_subject = history;
     }
     else if(strncmp(temp_subject, "Idræt", 4) == 0) {
-    trans_subject = phys_ed;
+        trans_subject = phys_ed;
     }
     else if(strncmp(temp_subject, "Klassenstid", 4) == 0) {
-    trans_subject = classtime;
+        trans_subject = classtime;
     }
     else if(strncmp(temp_subject, "Kristendom", 4) == 0) {
-    trans_subject = religion;
+        trans_subject = religion;
     }
     else if(strncmp(temp_subject, "Madkundskab", 4) == 0) {
-    trans_subject = cooking;
+        trans_subject = cooking;
     }
     else if(strncmp(temp_subject, "Matematik", 4) == 0) {
-    trans_subject = math;
+        trans_subject = math;
     }
     else if(strncmp(temp_subject, "Musik", 4) == 0) {
-    trans_subject = music;
+        trans_subject = music;
     }
     else if(strncmp(temp_subject, "Natur/teknik", 4) == 0) {
-    trans_subject = nature;
+        trans_subject = nature;
     }
     else if(strncmp(temp_subject, "Samfundsfag", 4) == 0) {
-    trans_subject = socialstud;
+        trans_subject = socialstud;
     }
     else if(strncmp(temp_subject, "Sløjd", 4) == 0) {
-    trans_subject = woodwork;
+        trans_subject = woodwork;
     }
     else if(strncmp(temp_subject, "Tysk", 4) == 0) {
-    trans_subject = german;
+        trans_subject = german;
     }
     else if(strncmp(temp_subject, "Valghold", 4) == 0) {
-    trans_subject = elective;
+        trans_subject = elective;
     }
-    
+
     return trans_subject;
 }
 
-//Function for generating weeks, by randomly inserting jobs in struct, removing teacher conflicts 
+//Function for generating weeks, by randomly inserting jobs in struct, removing teacher conflicts
 struct week* insert_jobs(int number_of_weeks) {
-    
+
     return 0;
 }
-
-
-
-
-
-
 
 //Function that generates a new population.
 //This function overwrites the initial population (thus returning void).
@@ -284,31 +278,12 @@ void next_generation(struct week* population_pool, unsigned int n) {
     return;
 }
 
-
-
 //Function for finding fitness of one week.
 int fitness_of_week(const struct week* individual) {
     return 0;
 }
 
-
-
 //Output the fittest of weeks in the population pool.
 void print_fittest_week(const struct week* population_pool) {
     return;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
